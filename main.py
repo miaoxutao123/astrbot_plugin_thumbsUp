b68a844db0e30def18c243d61f5b55a7b82a9d5b
miaoxutao123 <1025332440@qq.com>
2025-03-03 16:14:49 +0800

    第一次测试

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import json
 
 @register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
+        self.tunum = config.get("thnum")
+        self.response_str = config.get("response_str")
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.command("点赞")
     async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        num = self.tunum
+        response_str = self.response_str
+        if event.get_platform_name() == "aiocqhttp":
+            # qq
+            from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import AiocqhttpMessageEvent
+            assert isinstance(event, AiocqhttpMessageEvent)
+            client = event.bot # 得到 client
+            payloads = json.dumps({
+            "user_id": event.get_sender_id(),
+            "times": num
+            })
+            ret = await client.api.call_action('delete_msg', **payloads) # 调用 协议端  API
+            logger.info(f"delete_msg: {ret}")
+            yield event.plain_result(f"{response_str}")
